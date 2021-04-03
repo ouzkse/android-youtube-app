a84fb9a05b52baf8441c5b590071c033621f59ec
Oğuz Köse <oguzkose12@gmail.com>
2021-04-03 20:41:19 +0300

    Cancel ViewModelJob onCleared

--- app/src/main/java/android/app/ouzkse/youtube/data/base/BaseViewModel.kt
+++ app/src/main/java/android/app/ouzkse/youtube/data/base/BaseViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancel
 
 abstract class BaseViewModel : ViewModel() {
 
@@ -12,4 +13,9 @@ abstract class BaseViewModel : ViewModel() {
     lateinit var serviceFetchingJob: Job
 
     fun isFetching() = serviceFetchingJob.isActive
+
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
+    }
 }
