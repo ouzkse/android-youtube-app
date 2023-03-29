95aa51b0e685f23f109eb6602c9c1a208c73b755
Oğuz Köse <oguzkose12@gmail.com>
2023-03-29 21:37:49 +0300

    update multiple back-stack navigation graph

--- app/src/main/java/android/app/ouzkse/youtube/ui/MainActivity.kt
+++ app/src/main/java/android/app/ouzkse/youtube/ui/MainActivity.kt
@@ -2,57 +2,36 @@ package android.app.ouzkse.youtube.ui
 
 import android.app.ouzkse.youtube.R
 import android.app.ouzkse.youtube.databinding.ActivityMainBinding
-import android.app.ouzkse.youtube.util.setupWithNavController
 import android.content.Context
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.LiveData
 import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    var currentNavigationController: LiveData<NavController>? = null
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        if (savedInstanceState == null) {
-            setupBottomNavigationBar()
-        }
+        val navHostFragment = supportFragmentManager.findFragmentById(
+            R.id.navHostFragment
+        ) as NavHostFragment
+        navController = navHostFragment.navController
+
+        binding.bottomNavigationBar.setupWithNavController(navController)
     }
 
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-        setupBottomNavigationBar()
-    }
-
-    private fun setupBottomNavigationBar() {
-
-        val navigationGraphIds = listOf(
-            R.navigation.home_navigation_graph,
-            R.navigation.search_navigation_graph,
-            R.navigation.library_navigation_graph
-        )
-
-        val navController = binding.bottomNavigationBar.setupWithNavController(
-            navigationGraphIds,
-            supportFragmentManager,
-            binding.navHostFragment.id,
-            intent
-        )
-
-        currentNavigationController = navController
-    }
-
-    override fun onSupportNavigateUp(): Boolean =
-        currentNavigationController?.value?.navigateUp() ?: false
+    override fun onSupportNavigateUp(): Boolean = navController.navigateUp()
 
     fun hideKeyboard(view: View) {
         val imm: InputMethodManager =
